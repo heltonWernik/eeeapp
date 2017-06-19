fdb73c133177a4adf41aef2dee5df342c13bfdd3
“Helton <Your Actual Emailheltonwernik@gmail.com>
2017-06-19 10:18:46 -0300

    added a character

--- lib/eeeapp/web/elm/Game.elm
+++ lib/eeeapp/web/elm/Game.elm
@@ -1,6 +1,8 @@
 module Game exposing (..)
 
 import Html exposing (Html, div, text)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
 
 -- MAIN
@@ -21,12 +23,20 @@ main =
 
 
 type alias Model =
-    {}
+    { characterPositionX : Int
+    , characterPositionY : Int
+    , itemPositionX : Int
+    , itemPositionY : Int
+    }
 
 
 initialModel : Model
 initialModel =
-    {}
+    { characterPositionX = 50
+    , characterPositionY = 300
+    , itemPositionX = 500
+    , itemPositionY = 300
+    }
 
 
 init : ( Model, Cmd Msg )
@@ -64,4 +74,74 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    div [] [ text "Elm Game Base" ]
+    div [] [ viewGame model ]
+
+
+viewGame : Model -> Svg Msg
+viewGame model =
+    svg [ version "1.1", width "600", height "400" ]
+        [ viewGameWindow
+        , viewGameSky
+        , viewGameGround
+        , viewCharacter model
+        , viewItem model
+        ]
+
+
+viewGameWindow : Svg Msg
+viewGameWindow =
+    rect
+        [ width "600"
+        , height "400"
+        , fill "none"
+        , stroke "black"
+        ]
+        []
+
+
+viewGameSky : Svg Msg
+viewGameSky =
+    rect
+        [ x "0"
+        , y "0"
+        , width "600"
+        , height "300"
+        , fill "#4b7cfb"
+        ]
+        []
+
+
+viewGameGround : Svg Msg
+viewGameGround =
+    rect
+        [ x "0"
+        , y "300"
+        , width "600"
+        , height "100"
+        , fill "green"
+        ]
+        []
+
+
+viewCharacter : Model -> Svg Msg
+viewCharacter model =
+    image
+        [ xlinkHref "/images/character.gif"
+        , x (toString model.characterPositionX)
+        , y (toString model.characterPositionY)
+        , width "50"
+        , height "50"
+        ]
+        []
+
+
+viewItem : Model -> Svg Msg
+viewItem model =
+    image
+        [ xlinkHref "/images/coin.svg"
+        , x (toString model.itemPositionX)
+        , y (toString model.itemPositionY)
+        , width "20"
+        , height "20"
+        ]
+        []
